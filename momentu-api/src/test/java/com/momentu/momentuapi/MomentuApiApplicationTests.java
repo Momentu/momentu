7aefed54f7b44c9db83f0dc41f4c15a190cba775
Jerry Kobeszko <jerry.kobeszko@vivakinervecenter.com>
2017-10-09 13:58:40 -0500

    Initial momentu-api commit

---
+++ momentu-api/src/test/java/com/momentu/momentuapi/MomentuApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.momentu.momentuapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MomentuApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
