7dfadab6913c2d9fe1b47ca5b79a4482e494e39e
falharbi88 <32551407+falharbi88@users.noreply.github.com>
2017-10-11 14:37:10 -0500

    Welcome activity
    I designed  a welcome page activity with three buttons: Sign up, login, and term and policy. Each button coorsponding to a specific activity. with clicking on these button, it will start a new activity based on that button

---
+++ MomentuAndroid/app/src/main/java/com/momentu/momentuandroid/WelcomeActivity.java
@@ -0,0 +1,39 @@
+package com.momentu.momentuandroid;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class WelcomeActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        /*
+        TextView Fahad = (TextView) findViewById(R.id.text);
+        Fahad.setText("Fahad Alharbi\n"+"Ibrahim Alawwad\n"+"Jerry Kobeszko\n"+"Zhen Chen");
+        */
+    }
+
+    //On Click method to go to sign up activity
+    public void onClickSignUp(View v){
+        Intent signUp = new Intent(this, SignUpActivity.class);
+        startActivity(signUp);
+    }
+
+    //On Click method to go to sign up activity
+    public void onClickLogin(View v){
+        Intent login = new Intent(this, LoginActivity.class);
+        startActivity(login);
+    }
+
+    //On Click method to go to term and policy activity
+    public void onClickTermAndPolicy(View v){
+        Intent termAndPolicy = new Intent(this, TermAndPolicyActivity.class);
+        startActivity(termAndPolicy);
+    }
+
+}
