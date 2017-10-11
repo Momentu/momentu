4b73ac61d1118285440bfe724e05576423473c7c
ioalawwad <ibm.awwad@gmail.com>
2017-10-11 11:45:43 -0500

    I did some editing on the MainActivity class

--- MomentuAndroid/app/src/main/java/com/momentu/momentuandroid/MainActivity.java
+++ MomentuAndroid/app/src/main/java/com/momentu/momentuandroid/MainActivity.java
@@ -1,16 +1,16 @@
 package com.momentu.momentuandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView Fahad = (TextView) findViewById(R.id.text);
-        Fahad.setText("Fahad Alharbi".toString());
+        Fahad.setText("Fahad Alharbi\n"+"Ibrahim Alawwad\n"+"Jerry Kobeszko\n"+"Zhen Chen");
     }
 }
