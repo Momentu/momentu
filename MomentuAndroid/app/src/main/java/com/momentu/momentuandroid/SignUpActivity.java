4701b245ff71812078071d5e6afc98b919afff8a
falharbi88 <32551407+falharbi88@users.noreply.github.com>
2017-10-11 22:31:09 -0500

    adding registration activity
    I have completed simple sign up page. It shows a toast message if the all data fields are entered. otherwise, it shows a message which data missing

--- MomentuAndroid/app/src/main/java/com/momentu/momentuandroid/SignUpActivity.java
+++ MomentuAndroid/app/src/main/java/com/momentu/momentuandroid/SignUpActivity.java
@@ -2,12 +2,73 @@ package com.momentu.momentuandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    EditText usernameInput, passwordInput, firstNameInput, lastNameInput, emailInput;
+
+    //This string is to get the value of radio buttons
+    String gender="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        usernameInput = (EditText) findViewById(R.id.usernameInput);
+        passwordInput = (EditText) findViewById(R.id.passwordInput);
+        firstNameInput = (EditText) findViewById(R.id.firstNameInput);
+        lastNameInput = (EditText) findViewById(R.id.lastNameInput);
+        emailInput = (EditText) findViewById(R.id.emailInput);
+
+    }
+
+    //On click method for radio button and choosing gender type. It sets gender attribute with the user's choice
+    public void onClickRadioButton(View v){
+        Button radioButton = (Button) v;
+        gender = radioButton.getText().toString();
+    }
+
+    //On click method for sign up button
+    public void onClickSignUp(View v){
+
+        if (checkFields()){
+            Toast.makeText(this,"Registration is complete",Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    //Check method to check the correctness of all data attribute entered by users
+    public boolean checkFields(){
+
+        if(usernameInput.getText().toString().isEmpty()){
+            Toast.makeText(this,"Username can't be empty",Toast.LENGTH_LONG).show();
+            return false;
+        }
+        else if(passwordInput.getText().toString().isEmpty()){
+            Toast.makeText(this,"Password can't be empty",Toast.LENGTH_LONG).show();
+            return false;
+        }
+        else if(firstNameInput.getText().toString().isEmpty()){
+            Toast.makeText(this,"First name can't be empty",Toast.LENGTH_LONG).show();
+            return false;
+        }
+        else if(lastNameInput.getText().toString().isEmpty()){
+            Toast.makeText(this,"Last name can't be empty",Toast.LENGTH_LONG).show();
+            return false;
+        }
+        else if(emailInput.getText().toString().isEmpty()){
+            Toast.makeText(this,"Email name can't be empty",Toast.LENGTH_LONG).show();
+            return false;
+        }
+        else if(gender.toString().isEmpty()){
+            Toast.makeText(this,"Please choose you gender",Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
     }
 }
