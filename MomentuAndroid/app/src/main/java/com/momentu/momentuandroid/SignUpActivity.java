b40882f22941dbd102c0dc28c06ab871ae56bab2
falharbi88 <32551407+falharbi88@users.noreply.github.com>
2017-10-13 22:10:51 -0500

    Trying to add http connection

--- MomentuAndroid/app/src/main/java/com/momentu/momentuandroid/SignUpActivity.java
+++ MomentuAndroid/app/src/main/java/com/momentu/momentuandroid/SignUpActivity.java
@@ -9,9 +9,14 @@ import android.widget.Toast;
 
 import com.momentu.momentuandroid.Model.User;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.net.ssl.HttpsURLConnection;
 
@@ -20,7 +25,7 @@ public class SignUpActivity extends AppCompatActivity {
     EditText usernameInput, passwordInput, firstNameInput, lastNameInput, emailInput;
 
     //This string is to get the value of radio buttons
-    String gender="";
+    String gender = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +41,7 @@ public class SignUpActivity extends AppCompatActivity {
     }
 
     //On click method for radio button and choosing gender type. It sets gender attribute with the user's choice
-    public void onClickRadioButton(View v){
+    public void onClickRadioButton(View v) {
         Button radioButton = (Button) v;
         gender = radioButton.getText().toString();
     }
@@ -44,7 +49,7 @@ public class SignUpActivity extends AppCompatActivity {
     //On click method for sign up button
     public void onClickSignUp(View v) throws IOException {
 
-        if (checkFields()){
+        if (checkFields()) {
             User newUser = new User();
             newUser.setUsername(usernameInput.getText().toString());
             newUser.setPassword(passwordInput.getText().toString());
@@ -52,38 +57,35 @@ public class SignUpActivity extends AppCompatActivity {
             newUser.setLastName(lastNameInput.getText().toString());
             newUser.setGender(gender.toString());
             newUser.setEmail(emailInput.getText().toString());
-            Toast.makeText(this,newUser.toString(),Toast.LENGTH_LONG).show();
+            Toast.makeText(SignUpActivity.this, newUser.toString(), Toast.LENGTH_LONG).show();
         }
 
     }
 
     //Check method to check the correctness of all data attribute entered by users
-    public boolean checkFields(){
+    public boolean checkFields() {
 
-        if(usernameInput.getText().toString().isEmpty()){
-            Toast.makeText(this,"Username can't be empty",Toast.LENGTH_LONG).show();
+        if (usernameInput.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Username can't be empty", Toast.LENGTH_LONG).show();
             return false;
-        }
-        else if(passwordInput.getText().toString().isEmpty()){
-            Toast.makeText(this,"Password can't be empty",Toast.LENGTH_LONG).show();
+        } else if (passwordInput.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Password can't be empty", Toast.LENGTH_LONG).show();
             return false;
-        }
-        else if(firstNameInput.getText().toString().isEmpty()){
-            Toast.makeText(this,"First name can't be empty",Toast.LENGTH_LONG).show();
+        } else if (firstNameInput.getText().toString().isEmpty()) {
+            Toast.makeText(this, "First name can't be empty", Toast.LENGTH_LONG).show();
             return false;
-        }
-        else if(lastNameInput.getText().toString().isEmpty()){
-            Toast.makeText(this,"Last name can't be empty",Toast.LENGTH_LONG).show();
+        } else if (lastNameInput.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Last name can't be empty", Toast.LENGTH_LONG).show();
             return false;
-        }
-        else if(emailInput.getText().toString().isEmpty()){
-            Toast.makeText(this,"Email name can't be empty",Toast.LENGTH_LONG).show();
+        } else if (emailInput.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Email name can't be empty", Toast.LENGTH_LONG).show();
             return false;
-        }
-        else if(gender.toString().isEmpty()){
-            Toast.makeText(this,"Please choose you gender",Toast.LENGTH_LONG).show();
+        } else if (gender.toString().isEmpty()) {
+            Toast.makeText(this, "Please choose you gender", Toast.LENGTH_LONG).show();
             return false;
         }
         return true;
     }
+
+
 }
