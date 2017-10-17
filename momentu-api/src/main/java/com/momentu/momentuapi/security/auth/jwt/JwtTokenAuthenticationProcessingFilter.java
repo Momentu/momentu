347aa422e581053697ea04b8435b207cea1b5769
Jerry Kobeszko <jerry.kobeszko@vivakinervecenter.com>
2017-10-17 15:35:52 -0500

    AuthenticationProcessingFilter methods for attempting authentication along with handling successes and failures

--- momentu-api/src/main/java/com/momentu/momentuapi/security/auth/jwt/JwtTokenAuthenticationProcessingFilter.java
+++ momentu-api/src/main/java/com/momentu/momentuapi/security/auth/jwt/JwtTokenAuthenticationProcessingFilter.java
@@ -1,9 +1,13 @@
 package com.momentu.momentuapi.security.auth.jwt;
 
 import com.momentu.momentuapi.security.auth.jwt.extractor.TokenExtractor;
+import com.momentu.momentuapi.security.config.WebSecurityConfig;
+import com.momentu.momentuapi.security.model.token.RawAccessJwtToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.util.matcher.RequestMatcher;
@@ -27,16 +31,22 @@ public class JwtTokenAuthenticationProcessingFilter extends AbstractAuthenticati
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException, ServletException {
-        return null;
+        String httpHeaderAuthorizationValue = request.getHeader(WebSecurityConfig.JWT_TOKEN_HEADER_KEY);
+        RawAccessJwtToken token = new RawAccessJwtToken(tokenExtractor.extract(httpHeaderAuthorizationValue));
+        return getAuthenticationManager().authenticate(new JwtAuthenticationToken(token));
     }
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-
+        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+        securityContext.setAuthentication(authResult);
+        SecurityContextHolder.setContext(securityContext);
+        chain.doFilter(request, response);
     }
 
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
-
+        SecurityContextHolder.clearContext();
+        failureHandler.onAuthenticationFailure(request, response, failed);
     }
 }
