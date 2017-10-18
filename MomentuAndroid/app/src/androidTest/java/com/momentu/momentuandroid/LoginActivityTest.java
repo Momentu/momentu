d8c2968f3817b87cc6afb02caed019a69b38806d
falharbi88 <32551407+falharbi88@users.noreply.github.com>
2017-10-18 02:23:13 -0500

    login UI test
    adding UI tests for login activity

---
+++ MomentuAndroid/app/src/androidTest/java/com/momentu/momentuandroid/LoginActivityTest.java
@@ -0,0 +1,122 @@
+package com.momentu.momentuandroid;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.action.ViewActions.typeTextIntoFocusedView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasFocus;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class LoginActivityTest {
+
+    @Rule
+    public ActivityTestRule<WelcomeActivity> mActivityTestRule = new ActivityTestRule<>(WelcomeActivity.class);
+
+    @Test
+    public void loginActivityTest() {
+
+        ViewInteraction button = onView(
+                allOf(withId(R.id.loginButton), withText("Login"), isDisplayed()));
+        button.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.tWelcomeUser), withText("Momentu"),
+                        childAtPosition(
+                                allOf(withId(R.id.email_login_form),
+                                        childAtPosition(
+                                                withId(R.id.login_form),
+                                                0)),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(isDisplayed()));
+
+        ViewInteraction editText = onView(
+                allOf(withId(R.id.username),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.email_login_form),
+                                        1),
+                                0),
+                        isDisplayed()));
+        editText.perform(replaceText("fahad"));
+
+        ViewInteraction editText2 = onView(
+                allOf(withId(R.id.password),
+                        childAtPosition(
+                                childAtPosition(
+                                        withId(R.id.email_login_form),
+                                        2),
+                                0),
+                        isDisplayed()));
+        editText2.perform(replaceText("123456"));
+
+        ViewInteraction editText5 = onView(
+                withId(R.id.username));
+        editText5.perform(scrollTo(), replaceText("fahad"), closeSoftKeyboard());
+
+        ViewInteraction editText6 = onView(
+                withId(R.id.password));
+        editText6.perform(scrollTo(), replaceText("123456"), closeSoftKeyboard());
+
+        ViewInteraction button2 = onView(
+                allOf(withId(R.id.login_button), withText("Login"),
+                        withParent(allOf(withId(R.id.email_login_form),
+                                withParent(withId(R.id.login_form))))));
+        button2.perform(scrollTo(), click());
+
+    }
+    /*
+    @Test
+    public void clickLoginButton_WithNoUsernameAndPassowrd() {
+        //locate and click on the term and policy button
+        onView(withId(R.id.login_button)).perform(click());
+
+        //check if the the term and policy button screen is displayed by asserting that the term and policy text (TextView) is displayed
+        onView(withId(R.id.login_form)).check(matches(typeTextIntoFocusedView("This field is required")));
+    }
+*/
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
---
+++ MomentuAndroid/app/src/androidTest/java/com/momentu/momentuandroid/TermAndPolicyActivityTest.java
@@ -0,0 +1,90 @@
+package com.momentu.momentuandroid;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class TermAndPolicyActivityTest {
+
+    @Rule
+    public ActivityTestRule<WelcomeActivity> mActivityTestRule = new ActivityTestRule<>(WelcomeActivity.class);
+
+    @Test
+    public void termAndPolicyActivityTest() {
+        ViewInteraction button = onView(
+                allOf(withId(R.id.termAndPolicyButton), withText("By using Momentu, you agree to Momentu's"+"\n"+"Term of Use and Privacy Policy"), isDisplayed()));
+        button.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.term_and_policy_text_view), withText("Terms of Service"+"\n"+"\n"+"Welcome!"+"\n"+"\n"+"We’ve drafted these Terms of Service (which we call the “Terms”) so you’ll know the rules that govern our relationship with you. Although we have tried our best to strip the legalese from the Terms, there are places where these Terms may still read like a traditional contract. There’s a good reason for that: These Terms do indeed form a legally binding contract between you and Momentu Inc. So please read them carefully."+"\n"+"\n"+"By using Momentu or any of our other products or services that link to these Terms (we refer to these simply as the “Services”), you agree to the Terms. Of course, if you don’t agree with them, then don’t use the Services."+"\n"+"\n"+"ARBITRATION NOTICE: THESE TERMS CONTAIN AN ARBITRATION CLAUSE A LITTLE LATER ON. EXCEPT FOR CERTAIN TYPES OF DISPUTES MENTIONED IN THAT ARBITRATION CLAUSE, YOU AND MOMENTU INC. AGREE THAT DISPUTES BETWEEN US WILL BE RESOLVED BY MANDATORY BINDING ARBITRATION, AND YOU AND MOMENTU INC. WAIVE ANY RIGHT TO PARTICIPATE IN A CLASS-ACTION LAWSUIT OR CLASS-WIDE ARBITRATION."+"\n"+"\n"+"1. Who Can Use the Services"+"\n"+"\n"+"No one under 13 is allowed to create an account or use the Services. We may offer additional Services with additional terms that may require you to be even older to use them. So please read all terms carefully."+"\n"+"\n"+"By using the Services, you state that:"+"\n"+"\n"+"You can form a binding contract with Momentu Inc."+"\n"+"\n"+"You are not a person who is barred from receiving the Services under the laws of the United States or any other applicable jurisdiction—meaning that you do not appear on the U.S. Treasury Department’s list of Specially Designated Nationals or face any other similar prohibition."+"\n"+"\n"+"You will comply with these Terms and all applicable local, state, national, and international laws, rules, and regulations."+"\n"+"\n"+"If you are using the Services on behalf of a business or some other entity, you state that you are authorized to grant all licenses set forth in these Terms and to agree to these Terms on behalf of the business or entity. If you are using the Services on behalf of an entity of the U.S. Government, you agree to the Amendment to Momentu Inc. Terms of Service for U.S. Government Users."+"\n"+"\n"+"Contact us"+"\n"+"\n"+"We welcome comments, questions, concerns, or suggestions."+"\n"+"\n"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(isDisplayed()));
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.term_and_policy_text_view), withText("Terms of Service"+"\n"+"\n"+"Welcome!"+"\n"+"\n"+"We’ve drafted these Terms of Service (which we call the “Terms”) so you’ll know the rules that govern our relationship with you. Although we have tried our best to strip the legalese from the Terms, there are places where these Terms may still read like a traditional contract. There’s a good reason for that: These Terms do indeed form a legally binding contract between you and Momentu Inc. So please read them carefully."+"\n"+"\n"+"By using Momentu or any of our other products or services that link to these Terms (we refer to these simply as the “Services”), you agree to the Terms. Of course, if you don’t agree with them, then don’t use the Services."+"\n"+"\n"+"ARBITRATION NOTICE: THESE TERMS CONTAIN AN ARBITRATION CLAUSE A LITTLE LATER ON. EXCEPT FOR CERTAIN TYPES OF DISPUTES MENTIONED IN THAT ARBITRATION CLAUSE, YOU AND MOMENTU INC. AGREE THAT DISPUTES BETWEEN US WILL BE RESOLVED BY MANDATORY BINDING ARBITRATION, AND YOU AND MOMENTU INC. WAIVE ANY RIGHT TO PARTICIPATE IN A CLASS-ACTION LAWSUIT OR CLASS-WIDE ARBITRATION."+"\n"+"\n"+"1. Who Can Use the Services"+"\n"+"\n"+"No one under 13 is allowed to create an account or use the Services. We may offer additional Services with additional terms that may require you to be even older to use them. So please read all terms carefully."+"\n"+"\n"+"By using the Services, you state that:"+"\n"+"\n"+"You can form a binding contract with Momentu Inc."+"\n"+"\n"+"You are not a person who is barred from receiving the Services under the laws of the United States or any other applicable jurisdiction—meaning that you do not appear on the U.S. Treasury Department’s list of Specially Designated Nationals or face any other similar prohibition."+"\n"+"\n"+"You will comply with these Terms and all applicable local, state, national, and international laws, rules, and regulations."+"\n"+"\n"+"If you are using the Services on behalf of a business or some other entity, you state that you are authorized to grant all licenses set forth in these Terms and to agree to these Terms on behalf of the business or entity. If you are using the Services on behalf of an entity of the U.S. Government, you agree to the Amendment to Momentu Inc. Terms of Service for U.S. Government Users."+"\n"+"\n"+"Contact us"+"\n"+"\n"+"We welcome comments, questions, concerns, or suggestions."+"\n"+"\n"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.ScrollView.class),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textView2.check(matches(withText("Terms of Service"+"\n"+"\n"+"Welcome!"+"\n"+"\n"+"We’ve drafted these Terms of Service (which we call the “Terms”) so you’ll know the rules that govern our relationship with you. Although we have tried our best to strip the legalese from the Terms, there are places where these Terms may still read like a traditional contract. There’s a good reason for that: These Terms do indeed form a legally binding contract between you and Momentu Inc. So please read them carefully."+"\n"+"\n"+"By using Momentu or any of our other products or services that link to these Terms (we refer to these simply as the “Services”), you agree to the Terms. Of course, if you don’t agree with them, then don’t use the Services."+"\n"+"\n"+"ARBITRATION NOTICE: THESE TERMS CONTAIN AN ARBITRATION CLAUSE A LITTLE LATER ON. EXCEPT FOR CERTAIN TYPES OF DISPUTES MENTIONED IN THAT ARBITRATION CLAUSE, YOU AND MOMENTU INC. AGREE THAT DISPUTES BETWEEN US WILL BE RESOLVED BY MANDATORY BINDING ARBITRATION, AND YOU AND MOMENTU INC. WAIVE ANY RIGHT TO PARTICIPATE IN A CLASS-ACTION LAWSUIT OR CLASS-WIDE ARBITRATION."+"\n"+"\n"+"1. Who Can Use the Services"+"\n"+"\n"+"No one under 13 is allowed to create an account or use the Services. We may offer additional Services with additional terms that may require you to be even older to use them. So please read all terms carefully."+"\n"+"\n"+"By using the Services, you state that:"+"\n"+"\n"+"You can form a binding contract with Momentu Inc."+"\n"+"\n"+"You are not a person who is barred from receiving the Services under the laws of the United States or any other applicable jurisdiction—meaning that you do not appear on the U.S. Treasury Department’s list of Specially Designated Nationals or face any other similar prohibition."+"\n"+"\n"+"You will comply with these Terms and all applicable local, state, national, and international laws, rules, and regulations."+"\n"+"\n"+"If you are using the Services on behalf of a business or some other entity, you state that you are authorized to grant all licenses set forth in these Terms and to agree to these Terms on behalf of the business or entity. If you are using the Services on behalf of an entity of the U.S. Government, you agree to the Amendment to Momentu Inc. Terms of Service for U.S. Government Users."+"\n"+"\n"+"Contact us"+"\n"+"\n"+"We welcome comments, questions, concerns, or suggestions."+"\n"+"\n")));
+
+        ViewInteraction scrollView = onView(
+                allOf(childAtPosition(
+                        childAtPosition(
+                                withId(android.R.id.content),
+                                0),
+                        0),
+                        isDisplayed()));
+        scrollView.check(matches(isDisplayed()));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
