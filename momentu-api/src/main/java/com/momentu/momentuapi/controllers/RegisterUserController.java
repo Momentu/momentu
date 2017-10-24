258d0cb911aadaa2a13f5bf783962b841bb84b05
ioalawwad <ibm.awwad@gmail.com>
2017-10-23 19:00:46 -0500

    cleaner version of RegisterUserController

--- momentu-api/src/main/java/com/momentu/momentuapi/controllers/RegisterUserController.java
+++ momentu-api/src/main/java/com/momentu/momentuapi/controllers/RegisterUserController.java
@@ -54,47 +54,7 @@ public class RegisterUserController {
         UserRole roleAssociation = new UserRole(user.getId(), Role.MEMBER);
         userRoleRepository.save(roleAssociation);
 
-//        return ResponseEntity.ok(persistentEntityResourceAssembler.toResource(user));
+  //      return ResponseEntity.ok(persistentEntityResourceAssembler.toResource(user));
         return new ResponseEntity<User>(newUser, HttpStatus.OK);
     }
 }
-//
-//import com.momentu.momentuapi.entities.User;
-//import com.momentu.momentuapi.repos.UserRepository;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class RegisterUserController {
-//
-//    private UserRepository userRepository;
-//
-//    @Autowired
-//    public void setUserRepository(UserRepository userRepository) {
-//        this.userRepository = userRepository;
-//    }
-//
-//    @RequestMapping(value = "/")
-//    public ResponseEntity<User> get(){
-//
-//        User user1 = new User();
-//        user1.setFirstName("Ibrahim");
-//        user1.setLastName("Alawwad");
-//        user1.setEmail("Ialawwad@depaul.edu");
-//        user1.setGender("Male");
-//
-//        return new ResponseEntity<User>(user1, HttpStatus.OK);
-//    }
-//    @RequestMapping(value = "/register", method = RequestMethod.POST)
-//    public ResponseEntity<User> registerNewUser(@RequestBody User newUser){
-//        userRepository.save(newUser);
-//        System.out.println("Got in register method and got the user: " + newUser.getLastName());
-//        return new ResponseEntity<User>(newUser, HttpStatus.OK);
-//    }
-//}
