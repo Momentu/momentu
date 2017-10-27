c91ef6faf08139d905ba28d07f7459614c12a2ec
falharbi88 <32551407+falharbi88@users.noreply.github.com>
2017-10-27 17:39:35 -0500

    Adding ErrorResponseTest class
    I added Error response test class. It includes 6 test cases for all the method in that class.

---
+++ momentu-api/src/test/java/com/momentu/momentuapi/testCommon/ErrorResponseTest.java
@@ -0,0 +1,103 @@
+package com.momentu.momentuapi.testCommon;
+
+import com.momentu.momentuapi.common.ErrorCode;
+import com.momentu.momentuapi.common.ErrorResponse;
+import org.junit.Test;
+import org.springframework.http.HttpStatus;
+import static org.junit.Assert.*;
+
+//This is a test class for Error Response class
+public class ErrorResponseTest {
+
+    //This is a test object from Error Response class
+    ErrorResponse errorResponseTest;
+
+    //Testing Error Response constructor
+    @Test
+    public void testErrorResponse(){
+        try {
+            assertNull(errorResponseTest);
+            HttpStatus httpStatus = HttpStatus.OK;
+            String message = "Accepted";
+            ErrorCode errorCode = ErrorCode.JWT_TOKEN_EXPIRED;
+            errorResponseTest = new ErrorResponse(message, errorCode, httpStatus);
+            assertNotNull(errorResponseTest);
+        }catch (Exception e){
+            fail("Error response constructor doesn't work properly");
+        }
+    }
+
+    //Testing get http status method in Error Response class
+    @Test
+    public void testGetHttpStatus() {
+        try {
+            HttpStatus httpStatus = HttpStatus.OK;
+            String message = "Accepted";
+            ErrorCode errorCode = ErrorCode.JWT_TOKEN_EXPIRED;
+            errorResponseTest = new ErrorResponse(message, errorCode, httpStatus);
+            assertEquals(httpStatus.OK, errorResponseTest.getHttpStatus());
+
+        } catch (Exception e) {
+            fail("it is not getting http status value correctly");
+        }
+    }
+
+    //Testing get message method in Error Response class
+    @Test
+    public void testGetMessage(){
+        try {
+            HttpStatus httpStatus = HttpStatus.OK;
+            String message = "Accepted";
+            ErrorCode errorCode = ErrorCode.JWT_TOKEN_EXPIRED;
+            errorResponseTest = new ErrorResponse(message, errorCode, httpStatus);
+            assertEquals("Accepted", errorResponseTest.getMessage());
+        }catch (Exception e){
+            fail("it is not getting message value correctly");
+        }
+    }
+
+    //Testing get error code method in Error Response class
+    @Test
+    public void testGetErrorCode(){
+        try {
+            HttpStatus httpStatus = HttpStatus.OK;
+            String message = "Accepted";
+            ErrorCode errorCode = ErrorCode.JWT_TOKEN_EXPIRED;
+            errorResponseTest = new ErrorResponse(message, errorCode, httpStatus);
+            assertEquals(ErrorCode.JWT_TOKEN_EXPIRED, errorResponseTest.getErrorCode());
+        }catch (Exception e){
+            fail("it is not getting error code value correctly");
+        }
+    }
+
+    //Testing get time stamp method in Error Response class
+    @Test
+    public void testGetTimestamp(){
+        try {
+            HttpStatus httpStatus = HttpStatus.OK;
+            String message = "Accepted";
+            ErrorCode errorCode = ErrorCode.JWT_TOKEN_EXPIRED;
+            errorResponseTest = new ErrorResponse(message, errorCode, httpStatus);
+            assertNotNull(errorResponseTest.getTimestamp());
+        }catch (Exception e){
+            fail("it is not creating data object correctly");
+        }
+    }
+
+    //Testing of method in Error Response class
+    @Test
+    public void testOf(){
+        try {
+            ErrorResponse errorResponseTest2;
+            HttpStatus httpStatus = HttpStatus.OK;
+            String message = "Accepted";
+            ErrorCode errorCode = ErrorCode.JWT_TOKEN_EXPIRED;
+            errorResponseTest2 = new ErrorResponse(message, errorCode, httpStatus);
+            assertEquals(errorResponseTest2.getMessage(), errorResponseTest.of(message, errorCode, httpStatus).getMessage());
+        }catch (Exception e){
+            fail("static of method doesn't work properly in Error Response class");
+        }
+    }
+
+
+}
---
+++ momentu-api/src/test/java/com/momentu/momentuapi/testControllers/RegisterUserControllerTest.java
@@ -0,0 +1,23 @@
+package com.momentu.momentuapi.testControllers;
+
+import com.momentu.momentuapi.controllers.RegisterUserController;
+import org.junit.Test;
+import java.util.Objects;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+//This is a test class for register user controller class
+public class RegisterUserControllerTest {
+
+    //This is a test object from register user controller class
+    RegisterUserController registerUserControllerTest = new RegisterUserController();
+
+    //Testing register method in Register User Controller class
+    @Test
+    public void testRegister(){
+
+    }
+
+
+}
