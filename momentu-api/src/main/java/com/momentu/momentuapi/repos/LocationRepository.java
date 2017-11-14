a3badf9f66d0036db21d761e52ebbde7d873780f
Jerry Kobeszko <jerry.kobeszko@vivakinervecenter.com>
2017-11-14 12:09:49 -0600

    Fix for findByState endpoint

--- momentu-api/src/main/java/com/momentu/momentuapi/repos/LocationRepository.java
+++ momentu-api/src/main/java/com/momentu/momentuapi/repos/LocationRepository.java
@@ -7,13 +7,14 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
+import java.util.List;
 import java.util.Optional;
 
 @RepositoryRestResource
 public interface LocationRepository extends CrudRepository<Location, Long> {
 
     @Query("select l from Location l where l.state=:state")
-    Optional<Location> findByState(@Param("state") String state);
+    List<Location> findByState(@Param("state") String state);
 
     @Query("select l from Location l where l.city=:city and l.state=:state")
     Optional<Location> findByStateCity(@Param("city") String city, @Param("state") String state);
